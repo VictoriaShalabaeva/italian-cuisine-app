8f2fd8ace901858f66481a50ff22e160a90d131b
VictoriaShalabaeva <shalabaevavt@gmail.com>
2021-10-20 15:20:08 +0000

    Redirect to profile page after recipe edit

--- app.py
+++ app.py
@@ -151,6 +151,7 @@ def edit_recipe(recipe_id):
         }
         mongo.db.recipes.update({"_id": ObjectId(recipe_id)}, submit)
         flash("Recipe Successfully Updated")
+        return redirect(url_for("profile", username=session["user"]))
 
 
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
