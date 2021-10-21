27926aa958fe40ced44db428ff9332bdc771faf5
VictoriaShalabaeva <shalabaevavt@gmail.com>
2021-10-21 19:56:03 +0000

    Add modal to check before deleting recipe

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $("select").formSelect();
+    $('.modal').modal();
 
     validateMaterializeSelect();
     function validateMaterializeSelect() {
